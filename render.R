2aac9b64768ce2c449e7ceb3116a1986a555fbd9
chenmengkun <72289264+chenmengkun@users.noreply.github.com>
2021-11-27 12:09:26 -0500

    Update render.R

--- render.R
+++ render.R
@@ -7,7 +7,7 @@ rmarkdown::render(
     output_file   = output_file,
     output_dir    = "reports/",
     params        = list(
-        symbols        = 'AAPL',
+        symbols        = c('AAPL','MSFT','MRNA','BNTX','PFE'),
         show_code      = FALSE
     )
 )
